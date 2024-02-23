bf6e258cee059ce2f71ec560a7a51da8edc68ba1
Ali Alipour <spacealab@gmail.com>
2024-02-23 20:47:09 +0330

    updated and work

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,17 +1,19 @@
 "use client"
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
-// Assuming you have images in the public/images folder and their names are known
+import Image from 'next/image';
+
 const imagePaths = [
-  '/public/images/image1.jpg', // Example path for image1.jpg
-  '/public/images/image2.jpg', // Example path for image2.jpg
-  '/public/images/image3.jpg', // Example path for image3.jpg
-  '/public/images/image4.jpg', // Example path for image4.jpg
+  '/images/image1.jpg',
+  '/images/image2.jpg',
+  '/images/image3.jpg',
+  '/images/image4.jpg',
 ];
 
 const Slider: React.FC = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [isPaused, setIsPaused] = useState(false);
 
   const fetchNewImage = () => {
     setCurrentIndex((currentIndex + 1) % imagePaths.length);
@@ -21,21 +23,24 @@ const Slider: React.FC = () => {
     setCurrentIndex((currentIndex - 1 + imagePaths.length) % imagePaths.length);
   };
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (!isPaused) fetchNewImage();
+    }, 3000); // Change image every 3 seconds
+
+    return () => clearInterval(interval);
+  }, [currentIndex, isPaused]);
+
   return (
-    <div className="flex flex-col items-center justify-center p-4">
-      <img src={imagePaths[currentIndex]} alt="Random" className="shadow-lg rounded max-w-full h-auto align-middle border-none" />
-      <div className="flex mt-4">
-        <button
-          onClick={showPreviousImage}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-l"
-          disabled={currentIndex === 0}
-        >
+    <div className="flex flex-col p-4">
+      <div className="relative w-510 h-310" onMouseEnter={() => setIsPaused(true)} onMouseLeave={() => setIsPaused(false)}>
+        <Image width={510} height={310} src={imagePaths[currentIndex]} alt="Random" objectFit="cover" />
+      </div>
+      <div className="flex mt-4 justify-between">
+        <button onClick={showPreviousImage} className="bg-blue-500 hover:bg-blue-700 text-white font-bold w-[100px] py-2 rounded">
           Previous
         </button>
-        <button
-          onClick={fetchNewImage}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-r"
-        >
+        <button onClick={fetchNewImage} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 w-[100px] rounded">
           Next
         </button>
       </div>
@@ -44,5 +49,3 @@ const Slider: React.FC = () => {
 };
 
 export default Slider;
-
-
